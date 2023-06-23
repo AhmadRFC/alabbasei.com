5ecd0b6b04c865225822cbb9a6dbcd09ffec4c50
AhmadRFC <ahmad.productive@gmail.com>
2023-06-23 20:17:49 +0300

    implementing/configuring tailwind css

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__wrapper">
+        <div className="header__container">
+          <h1 className='text-blue-500 bg-gray-400'>Testtt</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
