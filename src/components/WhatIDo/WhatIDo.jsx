d0972ee0bf3953f548f03d0460781dc3710c1e2c
AhmadRFC <ahmad.productive@gmail.com>
2023-06-26 06:14:29 +0300

    building tailwindcss

--- src/components/WhatIDo/WhatIDo.jsx
+++ src/components/WhatIDo/WhatIDo.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import { AiFillAppstore } from 'react-icons/ai';
-import { FaMobile, FaGlobe } from 'react-icons/fa';
-import { SiProgress, SiAntdesign } from 'react-icons/si';
-import Title from '../Layouts/Title';
-import Card from './Card';
+import React from "react";
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe } from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+import Title from "../Layouts/Title";
+import Card from "./Card";
 
 const WhatIDo = () => {
   return (
-    <section id='WhatIDo' className='w-full pb-20 pt-16 border-b-[1px] border-b-black'>
-      <Title title='What I Do'/>
-      <div className='grid grid-cols-3 gap-16'>
-        <Card 
-        title='Bus Strat'
-        des='a bunch of text over here to acoomdeitanj fja fuf wejg sjdnsdfjbsgjsd gwehjgbwhje sd'
-        icon={<AiFillAppstore />}/>
-        <Card 
-        title='Bus Strat'
-        des='a bunch of text over here to acoomdeitanj fja fuf wejg sjdnsdfjbsgjsd gwehjgbwhje sd'
-        icon={<FaMobile />}/>
-        <Card 
-        title='Bus Strat'
-        des='a bunch of text over here to acoomdeitanj fja fuf wejg sjdnsdfjbsgjsd gwehjgbwhje sd'
-        icon={<FaGlobe />}/>
+    <section
+      id="WhatIDo"
+      className="w-full pb-20 pt-16 border-b-[1px] border-b-black"
+    >
+      <Title title="What I Do" />
+      <div className="grid grid-cols-3 gap-16">
+        <Card
+          title="Bus Strat"
+          des="a bunch of text over here to acoomdeitanj fja fuf wejg sjdnsdfjbsgjsd gwehjgbwhje sd"
+          icon={<AiFillAppstore />}
+        />
+        <Card
+          title="Bus Strat"
+          des="a bunch of text over here to acoomdeitanj fja fuf wejg sjdnsdfjbsgjsd gwehjgbwhje sd"
+          icon={<FaMobile />}
+        />
+        <Card
+          title="Bus Strat"
+          des="a bunch of text over here to acoomdeitanj fja fuf wejg sjdnsdfjbsgjsd gwehjgbwhje sd"
+          icon={<FaGlobe />}
+        />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default WhatIDo
+export default WhatIDo;
