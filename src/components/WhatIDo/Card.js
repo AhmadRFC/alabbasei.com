ad6d9f97fc6ea90826c96f922a89a8b3e0a4ac46
AhmadRFC <ahmad.productive@gmail.com>
2023-06-29 08:20:06 +0300

    updating browser icon & modifying banner

--- src/components/WhatIDo/Card.js
+++ src/components/WhatIDo/Card.js
@@ -4,7 +4,7 @@ import { BsArrowRightShort } from "react-icons/bs";
 const Card = ({ title, des, icon }) => {
   return (
     <div className="transform scale-y-100 hover:scale-y-105 transition-transform duration-300">
-      <div className="w-[100%] h-40 px-12 py-5 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-b from-bodyColor to-[#b5b5b5] group hover:bg-gradient-to-b hover:from-[#dedede] hover:to-[#b6b6b6] transition-colors duration-100 group">
+      <div className="w-50 h-36 px-6 py-4 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-b from-bodyColor to-[#b5b5b5] group hover:bg-gradient-to-b hover:from-[#dedede] hover:to-[#b6b6b6] transition-colors duration-100 group">
         <div className="flex h-full gap-10">
           <div>
             <span className="text-5xl text-designColor">{icon}</span>
